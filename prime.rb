57c6b83fc8aa3baa2116983faf1737c169a6f119
Christopher Osborn <z68ozz@gmail.com>
2020-03-11 03:53:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+# Addef prime?(num)
+  range = (2..num-1)
+    if num <= 0
+      return false
+    elsif range.any? {|x| num % x == 0}
+      return false
+    else
+      return true
+    end
+endd  code here!
